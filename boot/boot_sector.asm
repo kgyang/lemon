780505b9d888118d940d6b865afadb36592b05d4
kgyang <kaigeyang@sina.com>
2019-09-11 23:44:56 +0800

    boot_sector: print string

--- boot/boot_sector.asm
+++ boot/boot_sector.asm
@@ -1,18 +1,30 @@
+;
+; A boot sector
+;
 
-mov ah, 0x0e
+org 0x7c00 ;The code is loaded to 0x7c00
 
-mov al, 'H'
-int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'o'
-int 0x10
+mov bx, WELCOME_MSG
+call bios_print
 
 jmp $
+
+bios_print:
+    pusha
+    mov ah, 0x0e
+bios_print_loop:
+    mov al, [bx]
+    cmp al, 0
+    je bios_print_done
+    int 0x10
+    inc bx
+    jmp bios_print_loop
+bios_print_done:
+    popa
+    ret
+
+WELCOME_MSG:
+    db 'Welcome to Lemon', 0
 
 times 510-($-$$) db 0
 
