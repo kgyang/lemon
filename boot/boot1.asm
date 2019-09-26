9a61f147550828313636222abcbb979c0be1f1b8
kgyang <kaigeyang@sina.com>
2019-09-26 23:21:20 +0800

    boot: enter protected mode

--- boot/boot1.asm
+++ boot/boot1.asm
@@ -1,7 +1,8 @@
 ;
 ; A boot sector
 ;
-KERNELSTART equ 0x9000
+KERNELLOWER equ 0x9000
+KERNELUPPER equ 0x100000
 BOOT1START equ 0x1000
 STACKTOP equ 0x7000
 
@@ -11,48 +12,50 @@ start:
     mov sp, STACKTOP
 ;query the BIOS for the size of lower memory
 ;query the BIOS for the size of upper memory
-;read kernel sectors from the hard disk into lower memory
+;read kernel sectors from the hard disk into lower memory, each time 64KB due to segment limitation
     mov word [LBA_ADDR+2], 0x0080 ;128 blocks
-    mov word [LBA_ADDR+6], 0x0900 ;segment
-    mov dword [LBA_ADDR+8], 0x00000011 ;start from the second block
+    mov word [LBA_ADDR+6], 0x0900 ;destination segment
+    mov dword [LBA_ADDR+8], 0x00000012 ;start from the 19th block
 repeat:
     mov si, LBA_ADDR
     mov ah, 0x42
     mov dl, 0x80 ; hard disck, drive 0
     int 0x13
     jc load_kernel_done
-    add word [LBA_ADDR+4], 0x1000
+    add word [LBA_ADDR+6], 0x1000
     add dword [LBA_ADDR+8], 0x00000080
-    mov bx, LOAD_KERNEL_DONE
-    call bios_print
     jmp repeat
 
 load_kernel_done:
-    mov bx, LOAD_KERNEL_DONE
-    call bios_print
-    jmp $
+
 ;enable the A20 gate
+    mov ah, 0x24
+    int 0x15
+
 ;disable interrupt
     cli
 ;load the Global Descriptor Table
+    xor ax, ax
+    mov ds, ax
+    lgdt [GDTR]
 ;switch to protected mode
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+    jmp 0x08:pmmain
+
+bits 32
+pmmain:
+    mov ax, 0x10 ;data segment
+    mov ds, ax
+    mov ss, ax
+
+    mov byte [0xB8000], 'P' ; display P in left-up corner
+    mov byte [0xB8001], 0x1B
+
+    jmp $
 ;invoke the multiboot loader
 ;begin execution of the kernel
-
-; bios print function
-bios_print:
-    pusha
-    mov ah, 0x0e    ; int=10/ah=0x0e -> BIOS tele-type output
-bios_print_loop:
-    mov al, [bx]
-    cmp al, 0
-    je bios_print_done
-    int 0x10        ; print the character in al
-    inc bx
-    jmp bios_print_loop
-bios_print_done:
-    popa
-    ret
 
 ; data
 LBA_ADDR:
@@ -68,6 +71,24 @@ LOAD_KERNEL_DONE:
 
 LOAD_KERNEL_FAIL:
     db 'Load Kernel Fail', 0x0d, 0x0a, 0 ; followed by \r\n
+
+GDT:
+    dw 0,0,0,0 ;dummy
+
+    dw 0xFFFF  ;limit 0:15
+    dw 0x0000  ;base 0:15
+    dw 0x9A00  ;code read/exec access byte: present=1, privilege=0: base16:23
+    dw 0x00CF  ;granularity=4096 base24:31,flags, limit16:19
+
+    dw 0xFFFF  ;limit 0:15
+    dw 0x0000  ;base 0:15
+    dw 0x9200  ;data read/write access byte: present=1, privilege=0: base16:23
+    dw 0x00CF  ;granularity=4096 base24:31,flags, limit16:19
+GDT_END:
+
+GDTR:
+    dw GDT_END - GDT
+    dw GDT
 
 ; padding
 times 512*17-($-$$) db 0
